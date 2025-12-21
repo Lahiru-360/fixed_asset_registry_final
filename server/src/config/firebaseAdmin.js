564ba44ccd56e8669fa88090f708ab3859651bb8
Lahiru_360 <workinglahiru@gmail.com>
2025-12-21 18:17:31 +0530

    fix (firebase key)

--- server/src/config/firebaseAdmin.js
+++ server/src/config/firebaseAdmin.js
@@ -1,26 +1,10 @@
-/* import admin from "firebase-admin";
+import admin from "firebase-admin";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 // Parse service account from environment variable
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-export default admin;
-*/
-
-import admin from "firebase-admin";
-
-const raw = process.env.FIREBASE_SERVICE_ACCOUNT;
-if (!raw) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT missing in Heroku Config Vars");
-}
-
-const serviceAccount = JSON.parse(raw.replace(/\\n/g, "\n"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
